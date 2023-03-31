4330791ec9cd71f6484cc43ff04b9cd33051bcfd
Tyler M. Neher <tyler@neherdata.com>
2023-03-31 15:23:23 -0400

    temp
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- ndsweather.py
+++ ndsweather.py
@@ -30,11 +30,16 @@ datasets = [
 ]
 
 # Fetch the API
-forecasts = wk_client.fetch(datasets, 40.308392, -74.069771, "US", "US/Eastern")
+# forecasts = wk_client.fetch(datasets, 40.308392, -74.069771, "US", "US/Eastern")
 secretariat = wk_client.fetch(datasets, 40.308392, -74.069771, "US", "US/Eastern")
 
 # There is a convenience method for converting the forecast response object to JSON
-forecasts_json = forecasts.as_json()
+secretariat_json = secretariat.as_json()
 
-print(forecasts_json)
-print("Weather Data - Copyright © 2023 Apple Inc. All rights reserved. Apple Weather and Weather are trademarks of Apple Inc.")
+secretariat_weather = open("secretariat_weather.json", "w")
+
+
+print(secretariat_weather)
+print(
+    "Weather Data - Copyright © 2023 Apple Inc. All rights reserved. Apple Weather and Weather are trademarks of Apple Inc."
+)
